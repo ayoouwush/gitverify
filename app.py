01a72523b1a97d2d9a8a72d79bd7116adb615419
Ayush <ayoouwush@gmail.com>
2026-04-18 08:22:02 +0530

    made changes in app.py for deployment

--- app.py
+++ app.py
@@ -395,4 +395,5 @@ def download_portfolio(pid):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
